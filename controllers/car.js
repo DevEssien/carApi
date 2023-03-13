dd7659eb4aeb16235f053f4ef6cd5338668cb62a
Essien Emmanuel <essienemma300@gmail.com>
2023-03-13 01:52:37 +0100

    ignoring .env file

--- controllers/car.js
+++ controllers/car.js
@@ -1,14 +1,16 @@
+require("dotenv").config();
+
 const axios = require("axios");
 
 exports.getCars = async (req, res, next) => {
-    const url = "https://carapi.app/api/models";
+    const url = "https://carapi.app/api/makes";
     try {
         const response = await axios.get(url, {
             method: "GET",
             headers: {
                 "Content-Type": "application/json",
-                api_token: "361986bc-3d0f-46f4-8136-b57ba85488ee",
-                api_secret: "730ad4bc7e4e427b20baa438b5ec6d33",
+                api_token: process.env.API_TOKEN,
+                api_secret: process.env.API_SECRET,
             },
         });
         return res.status(response.status).json({
